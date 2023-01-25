0ceda7364259591a6f00f4b0e045cbefa6c3e1bb
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2023-01-25 16:51:46 -0500

    commit tree 34f56845aa2a631cf60d78d86c8a3404211846c3

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        name = "#{self.first_name} #{self.last_name}"
+        name
+    end
+
+    def list_roles
+         roles = self.characters
+         roles.map do |role|
+            "#{role.name} - #{Show.find(role.show_id).name}"
+        end
+    end
 end
